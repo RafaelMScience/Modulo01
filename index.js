aa0f93e8cc12dfe4f29e77ba57e1d88c58349c16
root <root@DESKTOP-CG0TOD9.localdomain>
2019-08-24 17:48:03 -0400

    Chore: Server.get #o que houve de mudanca?
    - foi mudado o server.res por server.get

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
 const express = require("express");
 
+const server = express();
+
+server.get("/teste", (req, res) => {
+  return res.json({ message: "Hello World" });
+});
+
 const server = express(server.listen(3000));
