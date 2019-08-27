f8c9c14d8e2ffe3bc1dd8dd62d068f375833e305
RafaelM <RafaelM.Science@gmail.com>
2019-08-27 19:13:00 -0300

    Add Crud
    #add crud i

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require("express");
 
 const server = express();
 
+server.use(express.json());
+
 //Query params = ?teste=1
 //Route params = /users/1
 //Request body ={ "name": "Diego", "email":"diego@gmail.com"}
@@ -10,14 +12,36 @@ const server = express();
 
 const users = ["Diego", "Claudio", "Victor"];
 
-server.get('/users', (req, res) => {
+server.get("/users", (req, res) => {
   return res.json(users);
-})
+});
 
 server.get("/users/:index", (req, res) => {
   const { index } = req.params;
 
   return res.json(users[index]);
+});
+
+server.post("/users", (req, res) => {
+  const { name } = req.body;
+
+  users.push(name);
+  return res.json(users);
+});
+
+server.put("/users/:index", (req, res) => {
+  const { index } = req.params;
+  const { name } = req.body;
+
+  users[index] = name;
+  return res.json(users);
+});
+
+server.delete("/users/:index", (req, res) => {
+  const { index } = req.params;
+  users.splice(index, 1); //splice percorre todo index
+
+  return res.json(users);
 });
 
 server.listen(3000);
