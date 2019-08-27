9132541c8e71a58ba785e4c933ceba706085d0b2
RafaelMScience <RafaelM.Science@gmail.com>
2019-08-27 18:41:47 -0300

    Add Post

--- index.js
+++ index.js
@@ -20,4 +20,12 @@ server.get("/users/:index", (req, res) => {
   return res.json(users[index]);
 });
 
-server.listen(3000);
+server.post('/users', (req, res) => {
+  const { name } = req.body;
+
+  users.push(name);
+  return res.json(users);
+
+}); 
+
+rver.listen(3000);
