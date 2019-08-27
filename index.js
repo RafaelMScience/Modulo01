7088058148ebe3df31ab77723d9141fed7fef92b
RafaelM <RafaelM.Science@gmail.com>
2019-08-27 11:24:02 -0300

    chore: Routes Params
    -Test Routes params

--- index.js
+++ index.js
@@ -2,8 +2,14 @@ const express = require("express");
 
 const server = express();
 
-server.get("/teste", (req, res) => {
-  return res.json({ message: "Hello World" });
+//Query params = ?teste=1
+//Route params = /users/1
+//Request body ={ "name": "Diego", "email":"diego@gmail.com"}
+
+server.get("/users/:id", (req, res) => {
+  const { id } = req.params;
+
+  return res.json({ message: `Buscando o usuario ${id}` });
 });
 
-const server = express(server.listen(3000));
+server.listen(3000);
